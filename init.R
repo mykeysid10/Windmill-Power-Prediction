5c9c4c98c02972f5939dedc802365a02ea0d636f
Siddharth Kulkarni <mykysid10@gmail.com>
2022-04-26 14:54:03 +0530

    Add files via upload

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("randomForest", "superml")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
